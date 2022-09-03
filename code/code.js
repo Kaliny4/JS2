ceadc31f4ef17bed699c51ca761f531d8b338638
Hanna Kalinichenko <hkali@softserveinc.com>
2022-09-03 10:07:49 +0300

    corrected

--- code/code.js
+++ code/code.js
@@ -1,53 +1,58 @@
-/*for(let i = 0; i < 30; ++i){
-    if (i === 0 || i ===29) {    
-        for (let j = 0; j < 29; ++j) {
-            document.write("*");
-        }    
-    
-      } else {
-  
-    console.group()
-    for(let j = 0; j < 30; ++j){
-        if(j === 0 || j === 29){
-         document.write("*")   
-        }else{
-         document.write("&nbsp;&nbsp;")  
+'use strict'
+/*Quadrat*/
+for(let i = 0; i < 20; i++){    
+        for (let j = 0; j < 38; j++) {
+            if(i === 0 || i === 19){
+                document.write("*");  
+        }else {
+            if(j === 0 || j === 19){
+                document.write("*");
+            }else {
+                document.write("&nbsp;&nbsp;&nbsp;&nbsp;");
+            }
         }
-         
-    }
-    console.groupEnd()
+        }
+  
     document.write("<br>")
 }
-}
 
-for(let i = 0; i < 30; ++i){
-       
-    for(let j = 0 + i; j < 30; ++j){
-        document.write("*")   
-        }
-         document.write("<br>")  
-        }
+/*Triangle*/
 
-
-for(let i = 0; i <30; i++){
-       
-    for(let j = 30 - i; j < 30; j++){
-        document.write("*")   
-        }
-         document.write("<br>")  
-        }
 for(let i = 0; i < 30; i++){
-       
-            for(let j = 0 + i; j < 30; j++){
-                document.write("*")   
+    for(let j = 0 + i; j < 30; j++){
+        document.write("&nbsp;");   
+        }
+        for (let b=(0+i+1); b>0; b--){
+            document.write("*");
+        }
+         document.write("<br>");  
+        }
+
+/*Romb*/
+for(let i = 0; i < 30; i++){
+    for(let j = 0 + i; j < 30; j++){
+        document.write("&nbsp;");   
+        }
+        for (let b=(0+i+1); b>0; b--){
+            document.write("*");
+        }
+         document.write("<br>");  
+        }
+for(let k = 0; k < 30; k++){
+            for(let j = 1 + k; j >0; j--){
+                document.write("&nbsp;");   
                 }
-                 document.write("<br>")  
+                for (let l=30-k; l>0; l--){
+                    document.write("*");
                 }
-                */
+                 document.write("<br>");  
+                }
+    document.write("<hr>");
+
 let n = 5;
-let num = parseInt(prompt("Enter the number", ""));
+let num = parseFloat(prompt("Enter the number", ""));
 if (Number.isInteger(num) && num % n === 0) {
-for (let i = 0; i <=num; i % n === 0) {
+for (let i = 0; i <=num; i = i+5) {
     console.log(i);}
 }
     else {
